9eb395929d825a8b5bf6fe838e158549fb326fe9
orangefei <1217387751@qq.com>
2023-07-18 10:28:32 +0800

    v1

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, render_template
+import requests
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+import time
+
+app = Flask(__name__)
+
+API_URL = 'https://api.anthropic.com/v1/complete'
+API_KEY = "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"
+
+@app.route("/chatbot", methods=['GET', 'POST'])
+def chatbot():
+    answer = ""
+    if request.method == 'POST':
+        user_input = request.form['prompt']
+        prompt = "\n\nHuman:" + user_input + "\n\nAssistant:"
+
+        data = {
+          "prompt": prompt,
+          "model": "claude-v1.3",
+          "max_tokens_to_sample": 5000  
+        }
+
+        headers = {
+          'Content-Type': 'application/json',
+          'x-api-key': API_KEY
+        }
+
+        response = requests.post(API_URL, json=data, headers=headers)
+
+        if response.status_code == 200:
+            result = response.json()
+
+            answer = result['completion']
+            answer = answer.replace('\n', '<br>')
+
+            if result.get('stream'):
+                # 打字机效果实现
+                pass
+
+            if answer.startswith('```'):
+                # 代码高亮实现
+                pass
+
+        else:
+            answer = "Error: API request failed"
+
+    return render_template('chatbot.html', answer=answer)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9999)
